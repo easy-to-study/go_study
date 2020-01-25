f66b19a45a591945fcc9e8ebfe3cacdff70ec841
sataga <t.g.s.0928@gmail.com>
2020-01-25 20:13:01 +0900

    一旦保存

--- practice/package/xlsx/sample3/main.go
+++ practice/package/xlsx/sample3/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
 
-	"github.com/tealeg/xlsx"
-	"github.com/sataga/go_study/qiita/excel/xlsx/sample3/pkg"
+	// "database/sql"
+	// "fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/tealeg/xlsx" // 実装した設定パッケージの読み込み
+	// "github.com/sataga/go_study/qiita/excel/xlsx/sample3/model"
+	"github.com/sataga/go_study/qiita/excel/xlsx/sample3/config"
 )
 
 type List struct {
@@ -58,7 +64,31 @@ func outputData(row *xlsx.Row, newsheet *xlsx.Sheet, r int) {
 }
 
 func main() {
-	model.DbConnect()
+	// 設定ファイルを読み込む
+	confDB, err := config.ReadConfDB()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// 設定値から接続文字列を生成
+	conStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", confDB.User, confDB.Pass, confDB.Host, confDB.Port, confDB.DbName, confDB.Charset)
+
+	// データベース接続
+	db, err := sql.Open("mysql", conStr)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// deferで処理終了前に必ず接続をクローズする
+	defer db.Close()
+
+	// 接続確認
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("データベース接続失敗")
+		return
+	} else {
+		fmt.Println("データベース接続成功！")
+	}
 }
 
 func test() {
