fc309722d50b12d86ba70e4c839cdc74f2625ce4
sataga <t.g.s.0928@gmail.com>
2020-01-26 08:06:11 +0900

    とりあえずcommit

---
+++ practice/go-training/go.mod
@@ -0,0 +1,5 @@
+module github.com/easy-to-study/go_study/practice/go-training
+
+go 1.13
+
+require github.com/go-sql-driver/mysql v1.5.0
---
+++ practice/go-training/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/easy-to-study/go_study/practice/go-training/conf"
+	"github.com/easy-to-study/go_study/practice/go-training/query"
+	_ "github.com/go-sql-driver/mysql"
+)
+ 
+func main() {
+ 
+    // 設定ファイルを読み込む
+    confDB, err := conf.ReadConfDB()
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+ 
+    // 設定値から接続文字列を生成
+    conStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", confDB.User, confDB.Pass, confDB.Host, confDB.Port, confDB.DbName, confDB.Charset)
+ 
+    // データベース接続
+    db, err := sql.Open("mysql", conStr)
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    // deferで処理終了前に必ず接続をクローズする
+    defer db.Close()
+ 
+    // INSERTの実行
+    id, err := query.InsertUser("USER-0001", "山田 hoge郎", "pass", db)
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    fmt.Printf("登録されたユーザのIDは【%d】です。\n", id)
+ 
+    // SELECTの実行
+    user, err := query.SelectUserById(id, db)
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    fmt.Printf("SELECTされたユーザ情報は以下の通りです。\n")
+    fmt.Printf("[ID] %s\n", user.Id)
+    fmt.Printf("[アカウント] %s\n", user.Account)
+    fmt.Printf("[名前] %s\n", user.Name)
+    fmt.Printf("[パスワード] %s\n", user.Passwd)
+    fmt.Printf("[登録日] %s\n", user.Created)
+ 
+}
